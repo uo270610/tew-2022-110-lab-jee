60f376d2cdbf986dd5b9233d4bacc3e513deaa6f
uo270610 <uo270610@uniovi.es>
2021-09-22 11:55:43 +0200

    TEW-2022-110-2.2-Servelts con sesiones.”

---  servletsv0_0/src/com/tew/Servlets/HolaMundoServlet.java
+++  servletsv0_0/src/com/tew/Servlets/HolaMundoServlet.java
@@ -2,6 +2,7 @@ package com.tew.Servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Vector;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -40,18 +41,39 @@ public class HolaMundoServlet extends HttpServlet {
     //proporcionados por profes
     public void doGet (HttpServletRequest request, HttpServletResponse response)
     		 throws IOException, ServletException {
-    	 String nombre = (String) request.getParameter("NombreUsuario");
-    	 response.setCharacterEncoding("UTF-8");
-    	 response.setContentType("text/html");
-    	 PrintWriter out = response.getWriter();
-    	 out.println("<HTML>");
-    	 out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
-    	 out.println("<BODY>");
+    	
+    	response.setCharacterEncoding("UTF-8");
+   	 	response.setContentType("text/html");
+   	 	PrintWriter out = response.getWriter();
+   	 	out.println("<HTML>");
+   	 	out.println("<HEAD><TITLE>Hola Mundo!</TITLE></HEAD>");
+   	 	out.println("<BODY>");
     	 
+    	 
+    	 
+    	 String nombre = (String) request.getParameter("NombreUsuario");
+    	 @SuppressWarnings("unchecked")
+    	 Vector<String> listado = 
+    	 (Vector<String>)request.getSession().getAttribute("listado");
+    	 if (listado == null){
+    	 listado = new Vector<String>();
+    	 }
     	 if ( nombre != null ){
     	 out.println("<br>Hola "+nombre+"<br>");
+    	 listado.addElement(nombre);
     	 }
-    	 out.println("Bienvenido a mi primera página Web!");
+    	 request.getSession().setAttribute("listado",listado);
+    	 out.println("Bienvenido a mi primera página web!");
+    	 out.println("<br>");
+    	 out.println("Contigo, hoy me han visitado:<br>");
+    	 for ( int i = 0 ; i < listado.size() ; i++ ){
+    	 out.println("<br>"+(String)listado.elementAt(i));
+    	 }
+    	 out.println("<a href=\"index.html\">volver</a>");
+
+    	 
+    	 
+    	 
     	 out.println("</BODY></HTML>");
     		}
 
