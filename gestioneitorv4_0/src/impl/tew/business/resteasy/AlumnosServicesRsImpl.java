a10d847a9ece6dddbe066406722877f7ae29c60a
Omega-lv <pablolvpablolv@gmail.com>
2021-11-10 16:45:31 +0100

    TEW-2022-110-7.3-Servicios: Implementación de servicio

---
+++ gestioneitorv4_0/src/impl/tew/business/resteasy/AlumnosServicesRsImpl.java
@@ -0,0 +1,35 @@
+package impl.tew.business.resteasy;
+import java.util.List;
+import com.tew.business.resteasy.AlumnosServicesRs;
+import com.tew.business.exception.EntityAlreadyExistsException;
+import com.tew.business.exception.EntityNotFoundException;
+import com.tew.model.Alumno;
+import impl.tew.business.classes.*;
+public class AlumnosServicesRsImpl implements AlumnosServicesRs {
+	@Override
+	public List<Alumno> getAlumnos() {
+	try {
+	return new AlumnosListado().getAlumnos();
+	} catch (Exception e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+	return null;
+	}
+	}
+	@Override
+	public void saveAlumno(Alumno alumno) throws EntityAlreadyExistsException {
+	new AlumnosAlta().save(alumno);
+	}
+	@Override
+	public void updateAlumno(Alumno alumno) throws EntityNotFoundException {
+	new AlumnosUpdate().update(alumno);
+	}
+	@Override
+	public void deleteAlumno(Long id) throws EntityNotFoundException {
+	new AlumnosBaja().delete(id);
+	}
+	@Override
+	public Alumno findById(Long id) throws EntityNotFoundException {
+	return new AlumnosBuscar().find(id);
+	}
+	}
